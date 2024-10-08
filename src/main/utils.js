645239800aec111aab1cd2241530d2a4095f2a10
sseezov <sseezov@mail.ru>
2024-10-09 00:11:56 +0700

    add pair parser

--- src/main/utils.js
+++ src/main/utils.js
@@ -1,17 +1,23 @@
 const lineEndings = process.platform === 'win32' ? '\r\n' : '\n';
 const shedule = {};
 
+const getPair = (time) => {
+  if (time < "09:00:00") return 'первая'
+  if (time < "10:45:00") return 'вторая'
+  if (time < "12:45:00") return 'третья'
+  if (time < "14:30:00") return 'четвертая'
+  if (time < "16:15:00") return 'пятая'
+  if (time < "19:00:00") return 'шестая'
+}
+
 const mapLessonToShedule = (lesson) => {
   const startTimeIndex = 4;
   const endTimeIndex = 5;
   const partisipantsIndex = 15;
 
   const lessonRows = lesson.split(lineEndings).slice(2).map((row) => row.split(','));
-  let [date, startTime] = lessonRows[0][startTimeIndex].split(' ');
-  date = date.slice(1);
-  startTime = startTime.slice(0, startTime.length - 1);
-  let endTime = lessonRows[0][endTimeIndex].split(' ')[1];
-  endTime = endTime.slice(0, endTime.length - 1);
+  const [date, startTime] = lessonRows[0][startTimeIndex].replace(/"/ig, '').split(' ');
+  const [, endTime] = lessonRows[0][endTimeIndex].replace(/"/ig, '').split(' ');
   const participants = lessonRows.map((row) => `${row[partisipantsIndex]}`).reduce((acc, elem) => acc.includes(elem) ? acc : acc.concat(elem), [])
   const table = [`ПЕРИОД ПАРЫ: ${startTime} - ${endTime}`, "УЧАСТНИКИ:", ...participants].join(lineEndings);
   shedule[date] ? shedule[date] = shedule[date].concat(`${lineEndings}${table}`) : shedule[date] = [`${lineEndings}${table}`];
