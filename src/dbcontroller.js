c35bd6ce067d3736a8ba8f6e6a2364ee904858ed
sseezov <sseezov@mail.ru>
2024-10-14 20:54:37 +0700

    tune db

--- src/dbcontroller.js
+++ src/dbcontroller.js
@@ -4,17 +4,11 @@ let sqlite = sqlite3.verbose()
 const db = new sqlite.Database('./db.sqlite3');
 
 db.serialize(() => {
-    db.run("CREATE TABLE sdfsdf (info TEXT)");
+    db.run("CREATE TABLE testdb (info TEXT, name TEXT)", [], (err) => {
+        console.log(err);
+    });
 
-    const stmt = db.prepare("INSERT INTO sdfsdf VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-
-    // db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-    //     console.log(row.id + ": " + row.info);
-    // });
+    db.run("INSERT INTO testdb VALUES (?, ?)", ['text', 'name1'], (e)=>console.log(e));
 });
 
 db.close();
